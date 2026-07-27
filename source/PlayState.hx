8d0dd35a80d9e6b6914cb5a46b9bde478c83df55
kiry vajiravuth <coolguy34393538@gmail.com>
2026-07-27 10:02:49 +0700

    add a version number on the botton right of the screen #5

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,19 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
 	override public function create()
 	{
+		var versionText = new FlxText(0, FlxG.height - 20, 0, "v0.1.0", 12);
+		versionText.setFormat(null, 12, 0xFFFFFF, RIGHT, OUTLINE, 0xFF000000);
+		versionText.scrollFactor.set(0, 0);
+		versionText.x = FlxG.width - versionText.width - 4;
+		versionText.alpha = 0.6;
+		add(versionText);
 		super.create();
-	}
-
-	override public function update(elapsed:Float)
-	{
-		super.update(elapsed);
 	}
 }
